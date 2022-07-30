b595e95da44044020a3bdfe22868fc30d13b9ca3
chapmandaniel <daniel.chapman@gmail.com>
2022-07-29 23:32:49 -0230

    created DisplayWord class for handling the letters/underscores output

---
+++ src/DisplayWord.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayWord {
+       private String word;
+       private List<Character> letters = new ArrayList<Character>();
+       private List<Character> guessedLetters = new ArrayList<Character>();
+
+    /**
+     *  use stringbuilder to create output string
+     *  check each letter, if it is in the guessed letters, display the letter, otherwise display an underscore
+     *
+     */
+}
